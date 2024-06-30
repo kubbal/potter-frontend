4991f1a2633e0fd7d5771458c6980b466eb9aed6
kubbal <kublerbalazs@gmail.com>
2024-06-30 11:06:58 +0200

    fix: map Date type for releaseDate

--- src/app/service/potter.service.ts
+++ src/app/service/potter.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { ApiConstants } from '../constants/api.constants';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { Book } from '../model/book.model';
 
 @Injectable({
@@ -13,6 +13,8 @@ export class PotterService {
   constructor(private http: HttpClient) { }
 
   getEnglishBooks(): Observable<Book[]> {
-    return this.http.get<Book[]>(environment.potterApiBaseUrl+ApiConstants.GET_EN_BOOKS);
+    return this.http.get<Book[]>(environment.potterApiBaseUrl+ApiConstants.GET_EN_BOOKS).pipe(
+      map(books => books.map(book => ({...book, releaseDate: new Date(book.releaseDate)})))
+    );
   }
 }
