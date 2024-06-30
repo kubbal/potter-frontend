d0b6c4dca6e8980873c9463dbd7eba0e0e01e06d
kubbal <kublerbalazs@gmail.com>
2024-06-30 11:05:45 +0200

    test: provide http client in app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [
+        provideHttpClient()
+      ]
     }).compileComponents();
   });
 
